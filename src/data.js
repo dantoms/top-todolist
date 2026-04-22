a38c6a9438fa1acb378343496ad9aef0630259a5
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-04-22 06:54:48 +0100

    Add project data structure

--- src/data.js
+++ src/data.js
@@ -1,9 +1,35 @@
 import { pubsub } from "./pubsub.js";
 
-
-
+class Projects {
+  constructor() {
+    this._projects = [];
   }
 
+  get projects() {
+    return this._projects;
+  }
+
+  addProject(project) {
+    this._projects.push(project);
+    pubsub.publish("projectAdded", this._projects);
+  }
+}
+
+export class Project {
+  constructor(name) {
+    this._name = name;
+    this._id = crypto.randomUUID();
+    projects.addProject(this);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get id() {
+    return this._id;
+  }
 }
 
 }
+export const projects = new Projects();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
 import { pubsub } from "./pubsub.js";
+import { Project, Task } from "./data.js";
 
+function dataRecieved(data) {
+  console.log(data);
+}
+
+pubsub.subscribe("projectAdded", dataRecieved);
+
+new Project("Inbox");
