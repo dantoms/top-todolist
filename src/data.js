4f34107560d2b3624ee7ff9e5911acdaabf5e8a4
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-04-22 06:52:23 +0100

    Start over

--- src/data.js
+++ src/data.js
@@ -1,32 +1,8 @@
-import { task } from "./tasks.js";
 
-export function getData() {
-  let data = localStorage.getItem("data");
-  if (data === null) {
-    data = {
-      projects: ["Inbox"],
-      tasks: [],
-    };
-    const newTask = task(
-      "Complete this task",
-      "desc",
-      "due",
-      "priority",
-      "Inbox",
-    );
 
-    data.tasks.push(newTask);
 
-    storeData(data);
-  } else {
-    data = JSON.parse(data);
   }
 
-  return data;
 }
 
-export function storeData(data) {
-  const dataString = JSON.stringify(data);
-  localStorage.setItem("data", dataString);
-  return "Data saved";
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
 import "./styles.css";
-import { getData } from "./data.js";
-import { displayProjects, displayTasks } from "./display.js";
-import { task } from "./tasks.js";
 
-const data = getData();
-displayProjects(data.projects);
-displayTasks(data.tasks);
-const newTask = task(
-  "New task created with factory",
-  "desc",
-  "due",
-  "priority",
-  "Inbox",
-);
-data.tasks.push(newTask);
-console.log(data);
-console.log(new Date(data.tasks[1].createdAt).toLocaleDateString());
